7d88e1fd3d2a88deb4d95a219ca45d6c6b4e580e
Harry Fitzgerald <fitzgerald_harry@network.lilly.com>
2018-02-01 11:45:33 +0000

    oi

--- src/server/routes/book.js
+++ src/server/routes/book.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const logger = require('../logger')('API Session 2');
+const logger = require('../logger')('AJAX Session');
 
 Array.prototype.random = function random() {
   const { length } = this;
@@ -15,7 +15,7 @@ const generateNonce = (len = 30) => {
   return new Array(len).fill(0).map(randChar).join('');
 };
 
-const key = generateNonce();
+let key = generateNonce();
 const fame = [];
 const prefixes = [
   'a good',
@@ -30,9 +30,9 @@ const prefixes = [
   'a (northern) Sam tier',
 ];
 
-// setInterval(() => {
-// key = generateNonce();
-// }, 5000);
+setInterval(() => {
+  key = generateNonce();
+}, 1500);
 
 const book = express.Router();
 
@@ -42,7 +42,7 @@ book.get('/tasks', (req, res) => {
       { name: 'Task 1', task: 'Make a request to /api/ajax2/YOURNAMEHERE, see it appear on the board and log the output', difficulty: 'Easy' },
       { name: 'Task 2', task: 'Create a function `printEndpoint` that takes one parameter, uses it as the argument for fetch and logs the output', difficulty: 'Medium' },
       { name: 'Task 3', task: 'Use the aforementioned function and access /api/ajax2/hall-of-fame to see who is in the hall of fame', difficulty: 'Easy' },
-      { name: 'Task 4', task: 'Make another function `getEasyTasks` to access /api/ajax2/tasks. Make it so that you can hit', difficulty: 'Medium' },
+      { name: 'Task 4', task: 'Make another function `getEasyTasks` to access /api/ajax2/tasks', difficulty: 'Medium' },
       { name: 'Task 5', task: 'Make a function that gets the secret key from /key/yourname', difficulty: 'Medium' },
       { name: 'Task 6', task: 'Make a request to get a key, via /api/ajax2/YOURNAMEGOESHERE, and when you get the key, make a POST request with your name and the key in the body', difficulty: 'Advanced' },
     ],
@@ -50,16 +50,13 @@ book.get('/tasks', (req, res) => {
   });
 });
 
-// task 1
-book.get('/:name', (req, res) => {
-  const prefix = prefixes.random();
-  logger(`${req.params.name} is ${prefix} coder!`);
-  res.json(`You're ${prefix} coder`);
-});
-
 // task 3
 book.get('/hall-of-fame', (req, res) => {
-  logger(`The following are in the hall of fame currently: ${fame.join('\n -')}`);
+  if (fame.length === 0) {
+    logger("No one's on the hall of fame yet!");
+    return res.json('No one is on it yet, will you be the first?');
+  }
+  logger(`The following are in the hall of fame: ${fame.join('\n -')}`);
   res.json(fame);
 });
 
@@ -88,5 +85,14 @@ book.post('/hall-of-fame', (req, res) => {
   }
   return res.json({ status: 'success', message: `Welcome back, ${name}` });
 });
+
+
+// task 1
+book.get('/:name', (req, res) => {
+  const prefix = prefixes.random();
+  logger(`${req.params.name} is ${prefix} coder!`);
+  res.json(`You're ${prefix} coder`);
+});
+
 
 module.exports = book;
