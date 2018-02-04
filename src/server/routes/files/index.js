19fa158d7931cca857790136e4301ce0ae30b97d
Harry <hfitz83@gmail.com>
2018-02-04 20:04:49 +0000

    cleanup

--- src/server/routes/files/index.js
+++ src/server/routes/files/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const multer = require('multer');
 const Redis = require('redis-utils-json');
-const { JSDOM } = require('jsdom');
-const { transformSync } = require('@babel/core');
-const { randomID } = require('./util');
+
+const { randomID, cleanDocument } = require('./util');
 const log = require('../../logger')('upload');
 
 const client = new Redis(process.env.REDIS_URL);
@@ -19,7 +18,7 @@ fileRouter.post('/', upload.single('index'), async (req, res) => {
   log(`Recieved file ${name}`);
   if (mimetype !== 'text/html') {
     log('Error with file');
-    return res.json({ status: 'error', message: 'Expected HTML file' });
+    return res.json({ status: 'error', message: `Expected HTML file, given ${mimetype}` });
   }
   // generate filename + keys for redis
   const id = randomID(15);
@@ -28,51 +27,12 @@ fileRouter.post('/', upload.single('index'), async (req, res) => {
   // extract head/body info using JSDOM
   // TODO: add polyfill
   const contents = buffer.toString();
-  const dom = new JSDOM(contents);
-  const head = dom.window.document.querySelector('head');
-  // check title and meta tags
-  const hasTitle = !!head.querySelector('title');
-  const meta = head.querySelectorAll('meta');
-  [...meta].forEach(tag => {
-    if (tag.outerHTML.match(/charset/gi)) {
-      log('Removing <meta charset />');
-      head.removeChild(tag);
-    }
-  });
-
-  // if no title, append one
-  if (!hasTitle) {
-    log('Creating title');
-    const title = dom.window.document.createElement('title');
-    title.innerHTML = filename;
-    head.appendChild(title);
-  }
-
-  // handle script polyfilling
-  const scripts = head.querySelectorAll('script');
-  scripts.forEach(script => {
-    const { innerHTML: code } = script;
-    const { code: polyfilled } = transformSync(code, { extends: `${__dirname}/.babelrc` });
-    head.removeChild(script);
-    const newScript = dom.window.document.createElement('script');
-    newScript.textContent = polyfilled;
-    head.appendChild(newScript);
-  });
-
-  // pull out the body
-  const body = dom.window.document.querySelector('body');
-  // how we're to store our formatted data
-  const formattedFile = {
-    head: head.innerHTML,
-    body: body.innerHTML,
-  };
+  log('Attempting to format file...');
+  const formattedFile = cleanDocument(contents, filename);
   // store it in redis!
-  try {
-    await client.setKey(key, formattedFile);
-    res.json({ status: 'success', filename });
-  } catch (err) {
-    res.json({ status: 'error', message: err });
-  }
+  log(`Done! Located at /api/files/${filename}`);
+  await client.setKey(key, formattedFile);
+  return res.json({ status: 'success', filename });
 });
 
 fileRouter.get('/:filename', async (req, res) => {
